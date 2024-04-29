074f249faed95763456c6d9f256e5253ff66c5e5
MaryanQ <maryansalad7@outlook.dk>
2024-04-29 21:07:12 +0200

    update

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ app.use(cookieParser());
 app.use(helmet());
 app.use(
   cors({
-    origin: "https://localhost:5173", // Ensure this exactly matches the client's origin
+    origin: "http://localhost:5173", // Ensure this exactly matches the client's origin
     methods: ["GET", "POST", "PUT", "DELETE"], // Adjust according to your needs
     credentials: true, // if you are using cookies or authentication
     allowedHeaders: ["Content-Type", "Authorization"], // Customize based on your requirements
